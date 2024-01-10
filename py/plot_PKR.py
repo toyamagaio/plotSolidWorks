13f5601edf2f0009f9e580e6a180a161617d0b00
toyama <toyama@isc.chubu.ac.jp>
2024-01-10 16:24:41 +0900

    update plot_PKR.py at MLF

--- py/plot_PKR.py
+++ py/plot_PKR.py
@@ -32,7 +32,8 @@ def format_time(dt):
 
 def plot_in_line_fromCSV(filename, outpdf, col_names=['Channel 1 [hPascal]'], interval_tlabel=5, log_flag=False):
 
-  df=pd.read_csv(filename, header=10,delimiter=';')
+  #df=pd.read_csv(filename, header=10,delimiter=';')
+  df=pd.read_csv(filename, header=11,delimiter=';')
   pp=PdfPages(outpdf)
   print(df)
   print(df.keys())
@@ -46,10 +47,13 @@ def plot_in_line_fromCSV(filename, outpdf, col_names=['Channel 1 [hPascal]'], in
   print(time_data)
   df['DateTime']=time_data
 
-  cut=df['DateTime']<datetime(2023,11,15,11,20)
+  #cut=df['DateTime']<datetime(2023,11,15,11,20)
+  #cut=df['DateTime']<datetime(2024,1,5,23,20)
+  cut=df['DateTime']<datetime(2024,1,7,23,20)
 
+  fig,ax=plt.subplots(figsize=(8,4.5))
   for col_name in col_names:
-    fig,ax=plt.subplots(figsize=(8,4.5))
+    #fig,ax=plt.subplots(figsize=(8,4.5))
     #fig.autofmt_xdate(rotation=45)
     #ax.plot(time_data,df[col_name], label=col_name)
     ax.plot(df['DateTime'],df[col_name], label=col_name)
@@ -64,24 +68,30 @@ def plot_in_line_fromCSV(filename, outpdf, col_names=['Channel 1 [hPascal]'], in
     ax.set_xticklabels([t.strftime('%m/%d %H:%M') for t in df['DateTime'][::interval_tlabel]], rotation=45, ha='center')
     if log_flag:
       ax.set_yscale('log')
-    fig.tight_layout()
-    pp.savefig(fig)
-    plt.close('all')
+  ax.grid(color = "gray", linestyle="--")
+  ax.axhline(y=1.33e-1, color='r')
+  fig.tight_layout()
+  pp.savefig(fig)
+  plt.close('all')
 
+  fig1,ax1=plt.subplots(figsize=(8,4.5))
+  for col_name in col_names:
     ##cut1
-    fig1,ax1=plt.subplots(figsize=(8,4.5))
     ax1.plot(df['DateTime'][cut],df[col_name][cut], label=col_name)
     ax1.set_ylabel(col_name)
     ax1.set_xlabel('Time')
     ax1.legend(ncol=2)
     time_data_cut=df['DateTime'][cut].copy()
-    ax1.set_xticks(time_data_cut[::interval_tlabel])
-    ax1.set_xticklabels([t.strftime('%m/%d %H:%M') for t in time_data_cut[::interval_tlabel]], rotation=45, ha='center')
+    #ax1.set_xticks(time_data_cut[::interval_tlabel])
+    #ax1.set_xticklabels([t.strftime('%m/%d %H:%M') for t in time_data_cut[::interval_tlabel]], rotation=45, ha='center')
+    ax1.set_xticks(time_data_cut[::20])
+    ax1.set_xticklabels([t.strftime('%m/%d %H:%M') for t in time_data_cut[::20]], rotation=45, ha='center')
     if log_flag:
       ax1.set_yscale('log')
-    fig1.tight_layout()
-    pp.savefig(fig1)
-    plt.close('all')
+  ax1.grid(color = "gray", linestyle="--")
+  fig1.tight_layout()
+  pp.savefig(fig1)
+  plt.close('all')
 
   pp.close()
 
@@ -90,7 +100,15 @@ def plot_single_along_axis():
   filename='DATALOG_20231115_102905.csv'
   outpdf='example.pdf'
   fname=datadir+'*.csv'
-  plot_in_line_fromCSV(datadir+filename,outpdf, log_flag=True)
+  #plot_in_line_fromCSV(datadir+filename,outpdf, log_flag=True)
+
+  filename='DATALOG_20240105_220800.csv'
+  outpdf='hpd107_20240105.pdf'
+  #plot_in_line_fromCSV(datadir+filename, outpdf, col_names=['Channel 1 [Pascal]'], interval_tlabel=60, log_flag=True)
+
+  filename='DATALOG_20240107_211306.csv'
+  outpdf='hpd107_20240107_doubleTMP.pdf'
+  plot_in_line_fromCSV(datadir+filename, outpdf, col_names=['Channel 1 [Pascal]', 'Channel 2 [Pascal]'], interval_tlabel=60, log_flag=True)
 
 if __name__=="__main__":
 
