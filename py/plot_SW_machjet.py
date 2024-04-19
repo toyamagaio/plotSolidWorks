f2a106c852192bd56961b94c16088d1d932c0265
toyama <toyama@isc.chubu.ac.jp>
2024-04-19 16:56:15 +0900

    update plot_SW_machjet.py: add compare method

--- py/plot_SW_machjet.py
+++ py/plot_SW_machjet.py
@@ -11,6 +11,38 @@ from matplotlib.backends.backend_pdf import PdfPages
 cmap=plt.get_cmap('tab10')
 from mpl_toolkits.mplot3d import Axes3D
 
+def compare_alongZ(files,labels,outpdf,outcsv=None):
+  fig, ax=plt.subplots(2,1,sharex='all')
+  fig1, ax1=plt.subplots()
+  df_out=pd.DataFrame()
+
+  for i,(filename,label) in enumerate(zip(files,labels)):
+    df=pd.read_csv(filename,delimiter='\t')
+    df['filename']=filename
+    df['label']=label
+    cut=(df['Z [mm]']>0.142592-0.01) & (df['Z [mm]']<0.142592+0.01)&(df['Y [mm]']>0.156903-0.01)&(df['Y [mm]']<0.156903+0.01) &(df['Pressure [Pa]']>0)
+    #cut=(df['Z [mm]']==0)&(df['Y [mm]']<0.1) &(df['Pressure [Pa]']>0)
+    ax[0].plot(df[cut]['X [mm]'],df[cut]['Pressure [Pa]'],label=label)
+    ax[1].plot(df[cut]['X [mm]'],df[cut]['Pressure [Pa]'],label=label)
+    ax1.hist(df[cut]['Y [mm]'],label=label, alpha=0.5)
+    df_out=pd.concat([df_out,df[cut]],axis=0,ignore_index=True)
+  ax[1].set_xlabel('X [mm]')
+  ax[0].set_ylabel('P [Pa]')
+  ax[0].legend()
+  ax[1].set_ylabel('P [Pa]')
+  ax[1].set_yscale('log')
+  ax1.legend()
+  ax1.set_xlabel('Y [mm]')
+  pp=PdfPages(outpdf)
+  pp.savefig(fig)
+  pp.savefig(fig1)
+  pp.close()
+  print('{} is saved.'.format(outpdf))
+
+  if outcsv!=None:
+    df_out.to_csv(outcsv)
+    print('{} is saved.'.format(outcsv))
+##########
 def plot_a_file_alongZ(filename,outpdf,stepy=1.0, dy=0.2):
   df=pd.read_csv(filename,delimiter='\t')
   print(df.keys())
@@ -60,6 +92,13 @@ def plot_a_file_alongZ(filename,outpdf,stepy=1.0, dy=0.2):
 
 if __name__=="__main__":
   
-  filename='../data/XZ_0.txt'
-  outpdf='../pdf/test.pdf'
-  plot_a_file_alongZ(filename,outpdf)
+  #filename='../data/XZ_0.txt'
+  #outpdf='../pdf/test.pdf'
+  #plot_a_file_alongZ(filename,outpdf)
+
+  files=['../data/No.8 (1Pa, 5MPa-1000Pa)/0928(1Pa, 5MPa-1000Pa)_0.txt', '../data/No.8 (1Pa, 5MPa-1000Pa)/0928(1Pa, 5MPa-1000Pa)_0.0000166.txt']
+  labels=['(1) t=0','(2) t=0.0000166']
+  outpdf='../pdf/test_compare.pdf'
+  outcsv='../data/test.csv'
+  compare_alongZ(files,labels,outpdf,outcsv=outcsv)
+  #compare_alongZ(files,labels,outpdf)
